87f6d7f0a3572d3d6b7feb8ce8a7acea7e5e4747
Tchinde Fosso <tchindefossomael@yahoo.fr>
2026-06-09 19:38:52 +0200

    fix: initialiser les locales fr_FR au démarrage (LocaleDataException)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
@@ -13,6 +14,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('fr_FR', null);
 
   // Firebase uniquement pour les notifications push FCM
   // L'app fonctionne sans si Firebase n'est pas configuré
